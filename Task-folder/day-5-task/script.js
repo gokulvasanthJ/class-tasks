dfaafc1b89706af023ec53647123c9effdc9799f
gokulvasanthJ <gokul27mech@gmail.com>
2023-03-31 09:06:48 +0530

    mesage commit

--- Task-folder/day-5-task/script.js
+++ Task-folder/day-5-task/script.js
@@ -1,12 +1,29 @@
 //Using Anonymous function;
 
-//print Odd numbers
-(numbers=>{
-    for( let i=0; i<numbers.length; i++){
-     if(i%2!=0){
-     console.log("OddNumbers",i)
-     }
-    }
-    })([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+//print Odd numbers:
+//1)
+// (numbers=>{
+//     for( let i=0; i<numbers.length; i++){
+//      if(i%2!=0){
+//      console.log("OddNumbers",i)
+//      }
+//     }
+//     })([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
     //
-    
+    //---------------X---------------
+    //Using function:
+    //1)
+    function numbers(){
+     let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+            for(  i=0; i<numbers.length; i++){
+                if(i%2!=0){
+                  console.log("OddNumbers",i)
+                  }
+                  
+               }           
+ }
+    numbers();
+    //---------------X-------------------
+
+
+   
